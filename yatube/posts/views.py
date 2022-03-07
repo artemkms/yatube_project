83e15257e582e7b5ae6fe53a0a93d90bf1d65d15
artemkms <69520287+artemkms@users.noreply.github.com>
2022-03-07 22:16:02 +0700

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_posts(request, slug):
+    return HttpResponse(f'Здесь будут посты, отфильтрованные по группам {slug}')
